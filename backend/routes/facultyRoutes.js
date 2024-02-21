e015ad0a78d87e52011d8961f93ac8cd4edb50a8
Swapnil Patel <swapnilpatel212601@gmail.com>
2024-02-22 00:50:59 +0530

    API AddOns

--- backend/routes/facultyRoutes.js
+++ backend/routes/facultyRoutes.js
@@ -1,10 +1,31 @@
-// routes/facultyRoutes.js
 
-const express = require("express");
-const facultyController = require("../controllers/facultyController");
+const express = require('express');
+
+const facultyController = require('../controllers/facultyController');
+const authMiddleware = require('../middleware/authMiddleware'); 
 
 const router = express.Router();
-router.get("/all", facultyController.getAllFaculties);
-router.get("/:id", facultyController.getFacultyById);
+
+// Fetching Faculty 
+router.get('/all', facultyController.getAllFaculties);
+router.get('/:id', facultyController.getFacultyById);
+
+//  Some Api Regarding Faculty Operation
+
+router.post('/createCourse', authMiddleware, facultyController.createCourse);
+
+router.post('/:course_id/addSubject', authMiddleware, facultyController.addSubject);
+
+router.post('/:subject_id/addLecture', authMiddleware, facultyController.addLecture);
+
+router.post('/:lecture_id/addQuiz', authMiddleware, facultyController.addQuiz);
+
+router.delete('/:course_id/removeStudent/:student_id', authMiddleware, facultyController.removeStudent);
+
+router.delete('/:course_id/removeSubject/:subject_id', authMiddleware, facultyController.removeSubject);
+
+router.delete('/:course_id/deleteCourse', authMiddleware, facultyController.deleteCourse);
+
+router.get('/getCoursesInfo', authMiddleware, facultyController.getCoursesInfo);
 
 module.exports = router;
