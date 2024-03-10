c3cd632b521cf504ce7d4ebe553ea3f976877191
Harsh Kumar <harshkumar371@gmail.com>
2024-03-10 19:33:45 +0530

    student-view

---
+++ student-view/src/components/sidebar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/sidebar.css'; 
+import logo from '../logo.svg'; 
+
+function Sidebar() {
+  return (
+    <div className="Sidebar">
+      <img src={logo} alt="Vit Vedant Logo" className="logo" />
+      <h2>Hi, Kapil</h2>
+      <p>MCA</p>
+      <button className="btn">My Subjects</button>
+      <button className="btn">No of Subjects</button>
+      <button className="btn">No of Lectures</button>
+    </div>
+  );
+}
+
+export default Sidebar;
